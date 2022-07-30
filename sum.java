3e8ecfce97ce9138df2a24ae2ca09b1f2aff7a5f
prakash chandravanshi <pkshch007@gmail.com>
2022-07-30 17:10:10 +0530

    added message in sum.java

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ public class sum{
     public static void main(String[] args) {
         int a=3;
         int b=4;
-        System.out.println(a+b);
+        System.out.println("the sum is :" +( a+b));
     }
 }
